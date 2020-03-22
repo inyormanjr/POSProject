833bb0427f6a51c0ae512aaa6481794852eea021
inyormanjr <55531614+inyormanjr@users.noreply.github.com>
2020-03-22 19:27:46 +1100

    Initial Checkin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { MainComponent } from './views/main/main.component';
+import { DashboardComponent } from './views/childview/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'main', component: MainComponent, children: [
+    {path: '', component: DashboardComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '**', component: DashboardComponent}
+  ]}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
